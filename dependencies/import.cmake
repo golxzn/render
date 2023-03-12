f913981ebbe9312c47d7fddaebb481e9136a4630
Ruslan Golovinsky <golxzn@gmail.com>
2023-03-12 10:27:52 +0400

    Import STB Image library

--- dependencies/import.cmake
+++ dependencies/import.cmake
@@ -21,10 +21,13 @@ add_subdirectory_params(${GOLXZNR_DEPENDENCIES_DIR}/glm
 	"BUILD_TESTING OFF"
 	"BUILD_STATIC_LIBS ON"
 )
+
+add_subdirectory(${GOLXZNR_DEPENDENCIES_DIR}/stb_image)
 add_dependencies(${target} glm::glm glad)
 
 list(APPEND dependencies
 	OpenGL::GL
+	stb::image
 	glad
 	glfw
 	golxzn::core
